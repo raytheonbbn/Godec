8c34e481f9e321fc6f77a53c827cc003c7972ecc
Ralf Meermeier <rmeermei@bbn.com>
2019-08-11 20:55:56 -0400

    Adds a Raspberry2 & 3 build

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,26 +1,27 @@
 #!/bin/bash -v
 set -e
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: run_tests.sh <installation directory>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: run_tests.sh <installation directory> <platform string, e.g. Linux, Android, Generic_ARM, Windows>"
     exit -1
 fi
 
 . $1/env.sh
 
-
 # Set to a semicolon-separated list of tests to be skipped
 skipped_tests="android.test"
 # Set to test in order to run just that one
 only_test=""
-if [ -x "$(command -v adb)" ] 
+if [ "$2" == "Android" ] 
 then
   skipped_tests=""
   only_test="android.test"
 fi
 
-if [ "$(expr substr $(uname -s) 1 9)" == "CYGWIN_NT" ]; then
-    skipped_tests="$skipped_tests;"
+if [ "$2" == "Generic_ARM" ] 
+then
+  skipped_tests=""
+  only_test="dummy.test"
 fi
 
 for reg_test in *.test; do
