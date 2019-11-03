ea46bb44f21c076366bff706be90bb0ccefd46ae
Ralf Meermeier <ralf.meermeer@gmail.com>
2019-11-03 23:37:16 +0000

    Add low latency support to soundcard reader

--- src/core_components/SoundcardRecorder.cc
+++ src/core_components/SoundcardRecorder.cc
@@ -404,10 +404,15 @@ void LinuxAudioRecorder::ProcessLoop() {
     while (mKeepRunning) {
         ret = snd_pcm_readi(capture_handle, audioBuffer, mChunkSize);
         if (ret < 0) {
+<<<<<<< HEAD
             if (ret == -EPIPE) {
                 std::cout << "CXRUN";
                 tttr("Couldn't prepare sound card", snd_pcm_prepare (capture_handle));
             } else if (ret == -EBADFD) GODEC_ERR << "Couldn't read audio, PCM is not in the right state";
+=======
+            if (ret == -EPIPE) std::cout << "CXRUN";
+            else if (ret == -EBADFD) GODEC_ERR << "Couldn't read audio, PCM is not in the right state";
+>>>>>>> Add low latency support to soundcard reader
             else if (ret == -ESTRPIPE) GODEC_ERR << "Couldn't read audio, suspend event occurred";
         }
         mGodecComp->receiveData(mChunkSize, mSamplingRate, mSampleDepth, mNumChannels, audioBuffer);
