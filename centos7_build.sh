c6cbcec2508320aeb6f421d352b3f9bc48b1b017
Andrew Tabarez <andrew.j.tabarez@raytheon.com>
2020-11-05 15:51:34 -0500

    Update centos7_build.sh

--- centos7_build.sh
+++ centos7_build.sh
@@ -13,9 +13,9 @@ cd $HOME/boost_1_68_0
 ./b2 clean 
 ./b2 -j8 -d0 cxxstd=17 cxxflags=-fPIC cflags=-fPIC --without-python link=static threading=multi threadapi=pthread define=BOOST_MATH_DISABLE_FLOAT128 
 export BOOST_ROOT=$HOME/boost_1_68_0 
-wget -q http://bitbucket.org/eigen/eigen/get/3.3.7.tar.bz2 
-tar jxf 3.3.7.tar.bz2 -C $HOME 
-export EIGEN_ROOT=$HOME/eigen-eigen-323c052e1731 
+wget -q http://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz
+tar xf eigen-3.3.7.tar.gz -C $HOME
+export EIGEN_ROOT=$HOME/eigen-3.3.7 
 export PYTHON_ROOT=/opt/rh/rh-python36/root/ 
 export PYTHON_HOME=/opt/rh/rh-python36/root/usr/ 
 export CMAKE_ARGS="-DBOOST_INCLUDEDIR=$BOOST_ROOT -DBOOST_LIBRARYDIR=$BOOST_ROOT/stage/lib/ -DEigen3_INCLUDE_DIRS=$EIGEN_ROOT -DPYTHON_LIBRARY=$PYTHON_ROOT/usr/lib64/libpython3.6m.so -DPYTHON_INCLUDE_DIR=$PYTHON_ROOT/usr/include/python3.6m -DGODEC_ADDITIONAL_INCLUDE_DIRS=$PYTHON_ROOT/usr/lib64/python3.6/site-packages/numpy/core/include/ " 
