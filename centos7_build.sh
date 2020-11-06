b4a1b8b41fe0d4c6044e1fbc2bd2de4b9ed95d16
Andrew Tabarez <atabarez@ld101.bbn.com>
2020-11-06 16:43:57 -0500

    Update centos build to use miniconda

--- centos7_build.sh
+++ centos7_build.sh
@@ -3,7 +3,10 @@ cd $HOME
 yum -y groupinstall 'Development Tools' 
 yum -y install centos-release-scl 
 yum -y install devtoolset-7-gcc\* 
-yum -y install libtool wget alsa-lib alsa-lib-devel rh-python36-python rh-python36-python-devel rh-python36-numpy java-1.8.0-openjdk java-1.8.0-openjdk-devel zlib-devel maven which 
+yum -y install libtool wget alsa-lib alsa-lib-devel java-1.8.0-openjdk java-1.8.0-openjdk-devel zlib-devel maven which
+curl -o Miniconda3.7.3.sh https://repo.anaconda.com/miniconda/Miniconda3-4.6.14-Linux-x86_64.sh &&bash Miniconda3.7.3.sh -b -p /conda
+export PATH=/conda/bin:$PATH
+conda install -y numpy
 export JAVA_HOME=$(realpath $(dirname $(which java))/..) 
 source /opt/rh/devtoolset-7/enable 
 wget -q https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.gz 
@@ -16,9 +19,9 @@ export BOOST_ROOT=$HOME/boost_1_68_0
 wget -q http://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz
 tar xf eigen-3.3.7.tar.gz -C $HOME
 export EIGEN_ROOT=$HOME/eigen-3.3.7 
-export PYTHON_ROOT=/opt/rh/rh-python36/root/ 
-export PYTHON_HOME=/opt/rh/rh-python36/root/usr/ 
-export CMAKE_ARGS="-DBOOST_INCLUDEDIR=$BOOST_ROOT -DBOOST_LIBRARYDIR=$BOOST_ROOT/stage/lib/ -DEigen3_INCLUDE_DIRS=$EIGEN_ROOT -DPYTHON_LIBRARY=$PYTHON_ROOT/usr/lib64/libpython3.6m.so -DPYTHON_INCLUDE_DIR=$PYTHON_ROOT/usr/include/python3.6m -DGODEC_ADDITIONAL_INCLUDE_DIRS=$PYTHON_ROOT/usr/lib64/python3.6/site-packages/numpy/core/include/ " 
+export PYTHON_ROOT=/conda
+export PYTHON_HOME=/conda
+export CMAKE_ARGS="-DBOOST_INCLUDEDIR=$BOOST_ROOT -DBOOST_LIBRARYDIR=$BOOST_ROOT/stage/lib/ -DEigen3_INCLUDE_DIRS=$EIGEN_ROOT -DPYTHON_LIBRARY=$PYTHON_ROOT/lib/libpython3.7m.so -DPYTHON_INCLUDE_DIR=$PYTHON_ROOT/include/python3.7m -DGODEC_ADDITIONAL_INCLUDE_DIRS=$PYTHON_ROOT/lib/python3.7/site-packages/numpy/core/include/ "
 cd $TRAVIS_BUILD_DIR && rm -rf cmake-build && mkdir cmake-build && cd cmake-build 
 "$MY_CMAKE" -DPLATFORM=$PLATFORM -DVERSION_STRING="$TRAVIS_TAG" $CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=../install ../ 
 eval "$MY_MAKE_COMMAND" 
